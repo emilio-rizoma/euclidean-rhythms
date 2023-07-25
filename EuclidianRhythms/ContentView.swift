f35bdf869ed2b25ca00cfbf612980afb096823a6
rival <emilio.parra.riz@gmail.com>
2023-07-24 23:01:51 -0600

    Initial Commit

---
+++ EuclidianRhythms/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  EuclidianRhythms
+//
+//  Created by Rival on 24/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+/System/Applications/Utilities/Terminal.app
---
+++ EuclidianRhythms/EuclidianRhythmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuclidianRhythmsApp.swift
+//  EuclidianRhythms
+//
+//  Created by Rival on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EuclidianRhythmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
